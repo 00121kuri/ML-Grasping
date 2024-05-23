093d40f7bb49c41b8c54c220c4aab3ec6c0dc6b4
00121kuri <00121kuri@gmail.com>
2024-05-23 18:53:25 +0900

    Add: 手の学習用のコンフィグを作成

--- python/run.sh
+++ python/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-run_id="RollerBall_$(date +'%Y-%m-%d-%H-%M-%S')"
-mlagents-learn ./config/RollerBall.yaml --run-id=$run_id --env=unity-build/unity-build --no-graphics --num-envs=16 &
+run_id="$(date +'%Y-%m-%d-%H-%M-%S')"
+# mlagents-learn ./config/one-hand.yaml --run-id=$run_id --env=unity-build/unity-build --no-graphics --num-envs=4 &
+mlagents-learn ./config/one-hand.yaml --run-id=$run_id
 tensorboard --logdir=results --port=6006 --host=0.0.0.0 &
 wait
