785e6ac49aab2e8099e05cfed9d25b22483d74c6
00121kuri <00121kuri@gmail.com>
2024-05-30 14:30:17 +0900

    Fix: run.shの細かな修正

--- python/run.sh
+++ python/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 run_id="$(date +'%Y-%m-%d-%H-%M-%S')"
 # mlagents-learn ./config/one-hand.yaml --run-id=$run_id --env=unity-build/unity-build --no-graphics --num-envs=4 &
-mlagents-learn ./config/one-hand.yaml --run-id=$run_id
+mlagents-learn ./config/one-hand.yaml --run-id=$run_id &
 tensorboard --logdir=results --port=6006 --host=0.0.0.0 &
 wait
