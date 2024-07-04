9a0ad4139e2b0000aa45f44d911e74e698614370
00121kuri <00121kuri@gmail.com>
2024-07-04 16:14:55 +0900

    Fix: tensorboardを常時起動するように変更

--- python/run.sh
+++ python/run.sh
@@ -2,5 +2,4 @@
 run_id="$(date +'%Y-%m-%d-%H-%M-%S')"
 # mlagents-learn ./config/one-hand.yaml --run-id=$run_id --env=unity-build/unity-build --no-graphics --num-envs=4 &
 mlagents-learn ./config/one-hand.yaml --run-id=$run_id &
-tensorboard --logdir=results --port=6006 --host=0.0.0.0 &
 wait
---
+++ python/tensorboard.sh
@@ -0,0 +1,1 @@
+tensorboard --logdir=results --port=6006 --host=0.0.0.0
