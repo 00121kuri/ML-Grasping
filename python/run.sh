26277de860e58fb18d45d33281ad48bb43f1105f
00121kuri <00121kuri@gmail.com>
2024-09-30 19:05:53 +0900

    Add: run.shから事前学習のidを指定できるように変更

--- python/run.sh
+++ python/run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-run_id="$(date +'%Y-%m-%d-%H-%M-%S')"
+default_run_id="$(date +'%Y-%m-%d-%H-%M-%S')"
+run_id=${1:-$default_run_id}
+echo "Using run_id: $run_id"
+
+if [ "$run_id" != "$default_run_id" ]; then
+    resume_option="--resume"
+    echo "Resuming previous training session"
+else
+    resume_option=""
+    echo "Starting new training session"
+fi
+
 # mlagents-learn ./config/one-hand.yaml --run-id=$run_id --env=unity-build/unity-build --no-graphics --num-envs=4 &
-mlagents-learn ./config/one-hand.yaml --run-id=$run_id &
+mlagents-learn ./config/one-hand.yaml --run-id=$run_id $resume_option &
 wait
