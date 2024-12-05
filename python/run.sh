6f3b53d26c1b1bf4a5ba7b3a83e20a61f25d6801
00121kuri <00121kuri@gmail.com>
2024-12-05 17:18:47 +0900

    Fix: 再学習のオプションを削除，run_idを指定できるように変更

--- python/run.sh
+++ python/run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-default_run_id="$(date +'%Y-%m-%d-%H-%M-%S')"
-run_id=${1:-$default_run_id}
-echo "Using run_id: $run_id"
+# 現在の日付をY-m-d形式で取得
+current_date=$(date +'%Y-%m-%d')
 
-if [ "$run_id" != "$default_run_id" ]; then
-    resume_option="--resume"
-    echo "Resuming previous training session"
+# 入力がある場合は入力と日付を結合、ない場合はデフォルトのタイムスタンプを使用
+if [ -n "$1" ]; then
+    run_id="${current_date}-${1}"
 else
-    resume_option=""
-    echo "Starting new training session"
+    run_id=$(date +'%Y-%m-%d-%H-%M-%S')
 fi
 
-mlagents-learn ./config/one-hand.yaml --run-id=$run_id $resume_option --env=build-app/ml-grasping --no-graphics --num-envs=4 &
-# mlagents-learn ./config/one-hand.yaml --run-id=$run_id $resume_option &
+echo "Using run_id: $run_id"
+
+mlagents-learn ./config/one-hand.yaml --run-id=$run_id --env=build-app/ml-grasping --no-graphics --num-envs=4 &
+# mlagents-learn ./config/one-hand.yaml --run-id=$run_id &
 wait
